641b6f91b1d5a162c34f1d33397e63c1af6a7e5a
Fuar11 <mcaa6556@gmail.com>
2024-12-18 15:48:52 -0500

    Added rose hip tea painkillers

--- Pain/PainPatches.cs
+++ Pain/PainPatches.cs
@@ -3,14 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MelonLoader;
 using UnityEngine;
 using Il2Cpp;
 using HarmonyLib;
-using Random = UnityEngine.Random;
-using System.Text.Json;
-using ImprovedAfflictions.Utils;
 using ImprovedAfflictions.Component;
+using Random = System.Random;
 
 
 namespace ImprovedAfflictions.Pain
@@ -31,7 +28,7 @@ namespace ImprovedAfflictions.Pain
 
         }
 
-        [HarmonyPatch(typeof(PlayerManager), nameof(PlayerManager.TreatAfflictionWithFirstAid))]
+        [HarmonyPatch(typeof(PlayerManager), nameof(PlayerManager.UseFirstAidItem))]
 
         public class PainkillerModifier
         {
@@ -39,11 +36,33 @@ namespace ImprovedAfflictions.Pain
             {
                 PainManager pm = Mod.painManager;
 
+                Mod.Logger.Log("Using first aid item", ComplexLogger.FlaggedLoggingLevel.Debug);
+
                 if (__instance.m_FirstAidItemUsed.name.ToLowerInvariant().Contains("painkiller"))
                 {
                     float amount = __instance.m_FirstAidItemUsed.m_GearItem.m_CurrentHP < 45 ? 20f * ((__instance.m_FirstAidItemUsed.m_GearItem.m_CurrentHP + 20) / 100) : 20f;
 
+                    Mod.Logger.Log("Big testo", ComplexLogger.FlaggedLoggingLevel.Debug);
+
                     pm.AdministerPainkillers(amount);
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(PlayerManager), nameof(PlayerManager.UseFoodInventoryItem))]
+
+        public class FeelTheRoses
+        {
+
+            public static void Postfix(PlayerManager __instance)
+            {
+                if (__instance.m_FoodItemEaten.name.ToLowerInvariant().Contains("rosehiptea"))
+                {
+                    Random rand = new Random();
+
+                    int amount = rand.Next(5, 10);
+
+                    Mod.painManager.AdministerPainkillers(amount);
                 }
 
             }
